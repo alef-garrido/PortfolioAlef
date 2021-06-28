074d1b6c6833b181cd9cd128815a5c667cc24d82
Alef Garrido <oa.garrido11@gmail.com>
2021-06-28 14:57:58 -0500

    Add html elements, js functions & css scrolling

---
+++ myfunctions.js
@@ -0,0 +1,14 @@
+// Mobile Menu
+function toggleFunction() {
+  const t = document.getElementById('links');
+  if (t.style.display === 'flex') {
+    t.style.display = 'none';
+  } else {
+    t.style.display = 'flex';
+  }
+}
+
+function b() {
+  document.getElementById('close-button').addEventListener('click', toggleFunction());
+  document.getElementById('menu').addEventListener('click', toggleFunction());
+}
